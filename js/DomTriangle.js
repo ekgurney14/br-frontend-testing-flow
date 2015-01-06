548aafdbb103a50ac757a9f0f608f8f2221a1bcb
Eric Gurney <ekgurney14@gmail.com>
2015-01-06 16:23:10 -0500

    made a change

--- js/DomTriangle.js
+++ js/DomTriangle.js
@@ -4,6 +4,7 @@ var DomTriangle = function(sideAElement, sideBElement, sideCElement) {
     return parseInt(sideAElement.value);
   };
 
+  
   this.sideBLength = function() {
     return parseInt(sideBElement.value);
   };
